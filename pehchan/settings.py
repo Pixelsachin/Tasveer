7328b2fc93684640a54aaf7d104a107e06bcd3e6
pixelsachin <bankchor23@gmail.com>
2024-08-27 23:37:23 +0530

    minor changes

--- pehchan/settings.py
+++ pehchan/settings.py
@@ -121,7 +121,7 @@ STATIC_URL = "static/"
 STATICFILES_DIR = [os.path.join(BASE_DIR, "static")]
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "images")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 
 # Default primary key field type
