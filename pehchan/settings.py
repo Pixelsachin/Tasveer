03a93d963df3559dc2b242fef009b048456cea31
pixelsachin <bankchor23@gmail.com>
2024-08-28 16:07:12 +0530

    minor change

--- pehchan/settings.py
+++ pehchan/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-tu+ge&+wcgy#s3_+c*ywli_yb&2r(ub7jzs^ma1*-$r^@!8c6#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["adharcard.onrender.com"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "adharcard.onrender.com"]
 
 
 # Application definition
